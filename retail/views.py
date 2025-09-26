69c5e600d53027eb6cc895a669283563832caf58
Nikita <example@mail>
2025-09-27 00:06:39 +0300

    commit

--- retail/views.py
+++ retail/views.py
@@ -1,26 +1,27 @@
-from rest_framework import viewsets, permissions
-from .models import TradingNetwork, Contact, Product
+from rest_framework import generics, permissions
+from .models import TradingNetwork
 from .serializers import TradingNetworkSerializer
 
 
 class IsActiveEmployee(permissions.BasePermission):
-
     def has_permission(self, request, view):
         return request.user and request.user.is_authenticated and request.user.is_active
 
 
-class TradingNetworkViewSet(viewsets.ModelViewSet):
-
+class TradingNetworkList(generics.ListCreateAPIView):
     queryset = TradingNetwork.objects.all()
     serializer_class = TradingNetworkSerializer
     permission_classes = [IsActiveEmployee]
 
     def get_queryset(self):
         queryset = super().get_queryset()
-
-        # Фильтрация по стране (требование задания)
         country = self.request.query_params.get("country")
         if country:
             queryset = queryset.filter(contacts__country__icontains=country)
-
         return queryset
+
+
+class TradingNetworkDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = TradingNetwork.objects.all()
+    serializer_class = TradingNetworkSerializer
+    permission_classes = [IsActiveEmployee]
